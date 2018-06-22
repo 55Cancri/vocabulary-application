700b0dd569439bcf0102a1605b40a7768904b683
Scott <sm.scottkmoore@gmail.com>
2018-06-22 10:19:24 -0400

    Now updating inputs via state change

--- client/app/components/modals/NewWordModal.tsx
+++ client/app/components/modals/NewWordModal.tsx
@@ -5,23 +5,69 @@ import { hideModal } from '../../actions/modal'
 
 interface IProps {
   hideModal: () => void
+  onChange: any
 }
 
-export class NewWordModal extends Component<IProps> {
+interface IState {
+  word: string,
+  tags: string,
+  topic: string,
+  definition: string
+}
+
+export class NewWordModal extends Component<IProps, IState> {
+  
+  state = {
+    word: '',
+    tags: '',
+    topic: '',
+    definition: ''
+  }
+  
   onClose = () => this.props.hideModal()
 
+  onFieldChange = e => {
+    let {name, value}: {name: keyof IState; value: string } = e.target
+    this.setState({
+      [name]: value
+    } as any)
+  }
+
+  addWord = () => {
+
+  }
+
   // @ts-ignore
-  render = () => (
+  render = () => {
+    const {word, tags, topic, definition} = this.state
+    return(
     <Modal onClose={this.onClose}>
-      <label htmlFor="word">Name</label>
-      <input type="text" name="word" />
-      <label htmlFor="definition">Definition</label>
-      <textarea name="definition" />
-      <label htmlFor="tags">Add tags</label>
-      <textarea name="tags" />
+      <form onSubmit={this.addWord}
+        onChange={this.onFieldChange} >
+        <div className="input-group" >
+          <label htmlFor="word">Name</label>
+          <input type="text" name="word" value={word} />
+        </div>
+        <div className="input-group" >
+          <label htmlFor="tags">Add tags</label>
+          <input type="text" name="tags" value={tags} />
+        </div>
+        <div className="input-group" >
+          <label htmlFor="topic">Add topic</label>
+          <input type="text" name="topic" value={topic} />
+        </div>
+        <div className="input-group" >
+          <label htmlFor="definition">Definition</label>
+          <textarea name="definition" value={definition} />
+        </div>
+        <div className="input-group" >
+          <input type="submit" name="submit" />
+        </div>
+      </form>
+      
       <p onClick={this.onClose}>click to close modal</p>
     </Modal>
-  )
+  )}
 }
 
 const mapDispatchToProps = dispatch => ({
