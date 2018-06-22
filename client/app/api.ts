8068a9966d918535f8384d24123317c10e3cb42d
Eric Morrison <malin1@protonmail.ch>
2018-06-22 15:37:29 -0400

    deleted unnecessary api calls

--- client/app/api.ts
+++ client/app/api.ts
@@ -38,53 +38,6 @@ export default {
         .post('/persist', { identity }, { headers: getToken() })
         .then(res => res.data),
 
-    submitReimbursement: reimbursement => {
-      return axios
-        .post('/submit', { reimbursement }, { headers: getToken() })
-        .then(res => res.data)
-    },
-
-    issueVerdicts: verdict => {
-      return axios
-        .post('/verdicts', { verdict }, { headers: getToken() })
-        .then(res => res.data)
-    },
-
-    updateGeneral: file =>
-      axios
-        .post('/api/general_settings', file, { headers: getToken() })
-        .then(res => res.data),
-
-    updatePassword: file =>
-      axios
-        .post('/api/password_settings', file, { headers: getToken() })
-        .then(res => res.data),
-
-    updateTransfer: file =>
-      axios
-        .post('/api/transfer_settings', file, { headers: getToken() })
-        .then(res => res.data),
-
-    deleteAccount: data =>
-      axios
-        .post('/api/delete', data, { headers: getToken() })
-        .then(res => res.data),
-
-    updateAccounts: data =>
-      axios
-        .post('/api/update', data, { headers: getToken() })
-        .then(res => res.data),
-
-    getUser: email =>
-      axios
-        .post('/api/get_user', email, { headers: getToken() })
-        .then(res => res.data),
-
-    deleteImage: data =>
-      axios
-        .post('/api/delete_image', data, { headers: getToken() })
-        .then(res => res.data),
-
     nuke: email =>
       axios
         .post('/api/nuke', email, { headers: getToken() })
