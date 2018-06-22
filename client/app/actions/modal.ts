29350c8ff4e3c720e6f75ab56ce98d2e2068f321
Scott <sm.scottkmoore@gmail.com>
2018-06-22 11:39:49 -0400

    Action for addWord should now send a word object to the API and then hide the modal

--- client/app/actions/modal.ts
+++ client/app/actions/modal.ts
@@ -6,7 +6,10 @@ export const loadModal = modalType => ({
 })
 
 export const addWord = word => dispatch =>
-  api.modal.addWord(word)
+  api.modal.addWord(word).then(
+    dispatch(hideModal())
+  )
+  .catch(err => console.log(err))
 
 // set modalType to null in redux store
 export const hideModal = () => ({
