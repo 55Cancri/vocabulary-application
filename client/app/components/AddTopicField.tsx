e923a0c250076352253f1afe66971142909a91d9
Scott <sm.scottkmoore@gmail.com>
2018-06-23 18:21:38 -0400

    Topic input field now updates its state

--- client/app/components/AddTopicField.tsx
+++ client/app/components/AddTopicField.tsx
@@ -1,36 +1,48 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, RouteComponentProps } from 'react-router-dom'
 import fontawesome from '@fortawesome/fontawesome'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 interface IProps {
-  // toggle: any
+  // onChange: any
 }
 
 interface IState {
-  isEditing: boolean
+  topic: string
 }
 
 export class AddTopicField extends Component<IProps, IState> {
 
   state = {
-    isEditing: false
+    topic: ''
   }
 
-  toggle = e => {
-    this.setState({isEditing: !this.state.isEditing})
-    console.log(`Switch state to ${this.state.isEditing}`)
+  onFieldChange = e => {
+    // let value: string = e.target
+    // this.setState({topic: value} as any)
+    let {name, value}: {name: keyof IState; value: string } = e.target
+    this.setState({
+      [name]: value
+    } as any)
+  }
+
+  handleSubmit = e => {
+
   }
 
   render() {
+    const topic = this.state.topic
     return (
       <div>
-        <input type="text" />
-        <button type="submit"
-          className="btn btn-primary btn-circle">
-          <FontAwesomeIcon icon="paper-plane" className="fa-paper-plane" />
-        </button>
+        <form onSubmit={this.handleSubmit}
+          onChange={this.onFieldChange} >
+          <input type="text" name="topic" value={topic} />
+          <button type="submit"
+            className="btn btn-primary btn-circle">
+            <FontAwesomeIcon icon="paper-plane" className="fa-paper-plane" />
+          </button>
+        </form>
       </div>
     )
   }
