ce0bae6bfbf1a1c075aafa08bc5ecb1170ea30ef
Eric Morrison <malin1@protonmail.ch>
2018-06-24 13:51:28 -0400

    about to pull

--- client/app/actions/app.ts
+++ client/app/actions/app.ts
@@ -5,19 +5,6 @@ export const startCreateWord = (nextWord, dispatch) => {
   console.log('Stepped into app.ts to addWord')
   api.modal.addWord(nextWord)
 }
-<<<<<<< HEAD
-=======
-export const startSubmitTopic = (nextTopic, dispatch) => {
-  api.sidebar.addTopic(nextTopic)
-}
-export const deleteAccount = user => ({
-  type: 'DELETE',
-  user
-})
-
-export const startDeleteAccount = data => dispatch =>
-  api.user.deleteAccount(data).then(user => dispatch(deleteAccount(user)))
->>>>>>> 38ca98a3bc5866823da2399f01e976b143b0e599
 
 export const nuke = user => ({
   type: 'NUKE',
