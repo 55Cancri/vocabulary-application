e56358af39b8a82b0340505574a5c219821b7cd7
Bino Philip <binophilip123@gmail.com>
2018-06-28 11:30:17 -0400

    profile styling

--- client/app/components/ProfilePage.tsx
+++ client/app/components/ProfilePage.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Axios from 'axios'
 import Dropzone from 'react-dropzone'
-import { demoAxios } from '../interceptors/demoAxios'
+import { stat } from 'fs'
 
 interface Iprops {
   username
@@ -10,27 +10,46 @@ interface Iprops {
   lastname
   email
   profileImage
+  topThree
+  words
 }
 
 export class ProfilePage extends Component<Iprops> {
   render() {
     return (
       <div className="profile-page">
-        <div className="input-group">
-          Profile Picture:
-          <p>
-            <img className="profile-image" src={this.props.profileImage} />
-            {/* <img src={this.state.url} /> */}
-          </p>
+        <div className="profile-pic-div">
+          <div
+            className="profile-image"
+            style={{
+              background: `url(${this.props.profileImage})`,
+              backgroundSize: 'contain',
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: '50% 50%',
+              width: 300,
+              height: 300
+            }}
+          />
         </div>
+
         <div className="input-group">
           <p className="title">
             Name: {this.props.firstname} {this.props.lastname}
           </p>
           <p className="title">Username: {this.props.username}</p>
           <p className="title">Email: {this.props.email}</p>
+          <p className="title">Wordcount: {this.props.words.length}</p>
         </div>
-        <div className="test-col">heloooooooooooo</div>
+
+        <div className="fav-words">
+          <h1>Favorite Words:</h1>
+          {this.props.topThree.map(word => (
+            <div className="fav-word">
+              <h2>{word.word} </h2>
+              <p>{word.definition}</p>
+            </div>
+          ))}
+        </div>
       </div>
     )
   }
@@ -41,7 +60,9 @@ const mapStateToProps = state => ({
   email: state.auth.email,
   firstname: state.auth.name,
   lastname: state.auth.last,
-  profileImage: state.auth.profileImage
+  profileImage: state.auth.profileImage,
+  topThree: state.lexica.words.slice(0, 3),
+  words: state.lexica.words
 })
 
 export default connect(mapStateToProps)(ProfilePage)
